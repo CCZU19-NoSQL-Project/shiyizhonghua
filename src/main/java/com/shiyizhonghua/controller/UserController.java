16fa8d2b32b47cd2b5f6c49e75dc42730842cef4
qyh540 <yhqian.01@foxmail.com>
2021-12-11 10:08:22 +0800

    SESSION_NAME为空解决

--- src/main/java/com/shiyizhonghua/controller/UserController.java
+++ src/main/java/com/shiyizhonghua/controller/UserController.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
 
     // session字段名
-    public static final String SESSION_NAME = null;
+    public static final String SESSION_NAME = "userData";
 
 
     @Resource
